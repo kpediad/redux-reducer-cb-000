98644cc1b5db44ba962a054ddf80aa9f3d67cfbf
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 16:01:13 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,9 +1,7 @@
 export function managePresents(state, action){
   switch (action.type) {
     case 'INCREASE':
-      return {count: state.count + 1}
-    case 'DECREASE_COUNT':
-      return {count: state.count - 1}
+      return {numberOfPresents: state.numberOfPresents + 1}
     default:
       return state;
   }
