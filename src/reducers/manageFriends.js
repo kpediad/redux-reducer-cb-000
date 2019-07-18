5a2842d4d6e1a625a3dc0c7d543189ec0f2c8fc4
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 16:10:51 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return {...state, friends.push(action.friend)}
+      return {friends: state.friends.push(action.friend)}
     case 'REMOVE_FRIEND':
       return {...state, friends.filter(i => i.id !== action.friend.id)}
     default:
