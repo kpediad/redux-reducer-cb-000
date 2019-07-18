e4ffa497ec66a5758895444f0ec162f84d639731
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 15:59:30 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {count: state.count + 1}
+    case 'DECREASE_COUNT':
+      return {count: state.count - 1}
+    default:
+      return state;
+  }
 }
